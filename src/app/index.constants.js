38eb7e76c3eea27a30dabff13b0e49c990797ba4
Oluwalayomi Akinrinade <layomia@gmail.com>
2017-07-15 11:54:19 -0700

    Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('kit')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
