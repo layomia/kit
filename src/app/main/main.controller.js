77e1d002961c4da92bd52fdea101a3e0eba5b2f5
Oluwalayomi Akinrinade <layomia@gmail.com>
2017-07-16 01:14:39 -0700

    Dummy temp data for artifacts rendering on home page

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,12 +6,22 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, $auth, $location, general) {
+  function MainController($timeout, $auth, $location, general, userInfo) {
     var vm = this;
+    vm.artifacts = [
+      {"name":"xbox 360", "imgUrl":"placeholder.png"},
+      {"name":"ps4", "imgUrl":"placeholder.png"},
+      {"name":"shoes", "imgUrl":"placeholder.png"},
+      {"name":"mouse", "imgUrl":"placeholder.png"},
+      {"name":"laptop", "imgUrl":"placeholder.png"},
+      {"name":"bottle", "imgUrl":"placeholder.png"},
+    ];
 
     if (!general.userLoggedIn()) {
       // flash message saying user isn't logged in
       $location.path("/");
     }
+
+    //console.log(general.currentUser());
   }
 })();
