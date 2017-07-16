8478d2ec31bb287c91c273ce14215d03fdd1fb57
Oluwalayomi Akinrinade <layomia@gmail.com>
2017-07-15 23:56:07 -0700

    Service for communities

---
+++ src/app/services/communityService.js
@@ -0,0 +1,36 @@
+angular.module('communityService', [])
+  .factory('Community', function($http) {
+
+    // create a new object
+    var communityFactory = {};
+    var communityRoute = "https://kitbackend.herokuapp.com/api/communities";
+
+    // get a single community
+    communityFactory.get = function(id) {
+      return $http.get(communityRoute + '/' + id);
+    };
+
+    // get all communities
+    communityFactory.all = function() {
+      return $http.get(communityRoute + '/');
+    };
+
+    // create a community
+    communityFactory.create = function(communityData) {
+      console.log("attempting to create");
+      return $http.post(communityRoute + '/', communityData);
+    };
+
+    // update a community
+    communityFactory.update = function(id, communityData) {
+      return $http.put(communityRoute + '/' + id, communityData);
+    };
+
+    // delete an community
+    communityFactory.delete = function(id) {
+      return $http.delete(communityRoute + '/' + id);
+    };
+
+    return communityFactory;
+
+  });
