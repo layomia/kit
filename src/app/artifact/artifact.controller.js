8651df3f34d8de79ae885ad1665a558e690924c2
Oluwalayomi Akinrinade <layomia@gmail.com>
2017-07-16 04:01:58 -0700

    Item page - first attempt

--- src/app/artifact/artifact.controller.js
+++ src/app/artifact/artifact.controller.js
@@ -12,16 +12,29 @@
       $location.path("/");
     }
     var vm = this;
+    vm.currentUser = general.currentUser();
+    vm.userCommunityId = vm.currentUser.community_id;
     vm.artifact = {};
     vm.artifactId = $stateParams.id;
+    vm.userOwnsArtifact = false;
+    vm.otherArtifacts = [];
 
     Artifact.get(vm.artifactId)
       .success(function(data) {
         vm.artifact = data;
         console.log(vm.artifact);
+        if (general.currentUser.id === vm.artifact.user_id) {
+          vm.userOwnsArtifact = true;
+        }
       })
       .error(function(data) {
         console.log("Trouble getting artifact");
+      })
+
+    Artifact.allForCommunity(vm.userCommunityId)
+      .success(function(data) {
+        vm.otherArtifacts = data.splice(0,3);
+        console.log(vm.otherArtifacts);
       })
 
   }
